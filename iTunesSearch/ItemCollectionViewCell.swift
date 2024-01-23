3b7c3e4d8f0552dcd8b4803f0f8a0bb19b7516db
Dax Gerber <dax.gerber@gmail.com>
2024-01-23 10:23:15 -0700

    first commit

---
+++ iTunesSearch/ItemCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCollectionViewCell.swift
+//  iTunesSearch
+//
+//  Created by Dax Gerber on 1/9/24.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell, ItemDisplaying {
+    
+    
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+}
